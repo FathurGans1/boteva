f35b56d917a40ffa9e0c1dc36070823af7e1443b
ANIRUDH <anirudhsudheer@gmail.com>
2021-12-02 07:11:08 +0530

    Initial commit

---
+++ plugins/igstalk.js
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `Example:\n${usedPrefix + command} ani._.rudh_s`
+
+  let res = await fetch(`https://fxc7-api.herokuapp.com/api/stalk/ig?apikey=pnj8NAJb&username=${args}`)
+   if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if(!json.result) throw json
+  let { full_name, username, followers, following, posts_count, is_private, is_verified, biography, profile_url} = json.result
+let spotifyinfo = `🎯 *Name:* ${full_name}
+👥 *Following:* ${followers}
+👩‍👩‍👧‍👧 *Followers:* ${following}
+📮 *Total Post:* ${posts_count}
+🔒 *Account Type:* ${is_private} 
+📜 *Bio:* ${biography}
+🔗 *Profile URL:* https://www.instagram.com/${username}
+`
+
+  await conn.sendFile(m.chat, profile_url, '', spotifyinfo, m)
+}  
+handler.help = ['igstalk <username>']
+handler.tags = ['tools']
+handler.command = /^(igstalk)$/i
+handler.limit = true
+module.exports = handler
---
+++ plugins/play.js
@@ -0,0 +1,40 @@
+const { servers, yta, ytv } = require('../lib/y2mate')
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `uhm.. what are you looking for?\n\nExample:\n${usedPrefix + command} Bad Boy`
+  let chat = global.db.data.chats[m.chat]
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw 'Content Not found'
+  let isVideo = /2$/.test(command)
+  let yt = false
+  let yt2 = false
+  let usedServer = servers[0]
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\ntry again...'}`)
+    }
+  }
+  let { dl_link, thumb, title, filesize, filesizeF } = yt
+  await conn.send3ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*Title:* ${title}
+*Audio File Size:* ${filesizeF}
+*Video File Size:* ${yt2.filesizeF}
+*Play Doesnt Work ,In Disappearing mode( Private Chat)*
+  `.trim(), watermark,'ℹ️Source', `.yts${vid.url}`, '📻Audio', `.yta ${vid.url}`, '🎥Video', `.yt ${vid.url}`)
+}
+handler.help = ['song','play','?'].map(v => v + ' <query>')
+handler.tags = ['downloader']
+handler.command = /^(play|song)$/i
+
+handler.exp = 0
+
+module.exports = handler
+
